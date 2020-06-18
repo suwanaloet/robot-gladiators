98c848eedd7f57be10af9dde107d5f3be71618bd
Sean Suwanaloet <suwanaloet@wisc.edu>
2020-06-18 14:00:45 -0700

    3.6 fin

--- assets/js/game.js
+++ assets/js/game.js
@@ -37,27 +37,25 @@ var startGame = function () {
     var shop = function () {
         //ask player what they want to do
         var shopOptionPrompt = window.prompt(
-            "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
-        );
+            "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
+          );
         // use switch to carry out action
+        shopOptionPrompt = parseInt(shopOptionPrompt);
         switch (shopOptionPrompt) {
-            case "REFILL": // new case
-            case "refill":
-                playerInfo.refillHealth();
-                break;
-            case "UPGRADE": // new case
-            case "upgrade":
-                playerInfo.upgradeAttack();
-                break;
-            case "LEAVE": // new case
-            case "leave":
-                window.alert("Leaving the store.");
-                break;
+            case 1:
+              playerInfo.refillHealth();
+              break;
+            case 2:
+              playerInfo.upgradeAttack();
+              break;
+            case 3:
+              window.alert("Leaving the store.");
+              break;
             default:
-                window.alert("You did not pick a valid option. Try again.");
-                shop();
-                break;
-        }
+              window.alert("You did not pick a valid option. Try again.");
+              shop();
+              break;
+          }
     };
     //rest is static:
     for (var i = 0; i < enemyInfo.length; i++) {
